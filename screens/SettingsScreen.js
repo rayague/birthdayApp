b826db57318b2356e282716216aa685bf3701703
Ray Ague <rayague03@gmail..com>
2024-07-04 04:23:20 +0100

    second commit

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,10 +1,21 @@
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  Pressable
+} from "react-native";
 import React from "react";
 import { BlurView } from "expo-blur";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { useNavigation } from "@react-navigation/native";
 
 const image = require("../assets/images/picture10.jpg");
 
 export default function SettingScreen() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -17,7 +28,36 @@ export default function SettingScreen() {
         style={styles.image}
       ></ImageBackground>
       <BlurView intensity={90} tint="dark" style={styles.blurContainer}>
-        <Text style={[styles.text, { color: "#fff" }]}>Vos Paramètres ici</Text>
+        <View style={styles.itemContainer}>
+          <Icon name="account" size={24} color="white" style={styles.icon} />
+          <Text style={[styles.text, { color: "#fff" }]}>FIRST NAME</Text>
+        </View>
+        <View style={styles.itemContainer}>
+          <Icon name="account" size={24} color="white" style={styles.icon} />
+
+          <Text style={[styles.text, { color: "#fff" }]}>LAST NAME</Text>
+        </View>
+        <View style={styles.itemContainer}>
+          <Icon name="account" size={24} color="white" style={styles.icon} />
+
+          <Text style={[styles.text, { color: "#fff" }]}>EMAIL</Text>
+        </View>
+        <View style={styles.itemContainer}>
+          <Icon name="account" size={24} color="white" style={styles.icon} />
+
+          <Text style={[styles.text, { color: "#fff" }]}>PASSWORD</Text>
+        </View>
+        <Pressable
+          style={styles.editButton}
+          onPress={() =>
+            navigation.navigate("Profile", { name: "ProfileScreen" })
+          }
+        >
+          <Text style={[styles.text, { color: "#fff" }]}>EDIT</Text>
+        </Pressable>
+        <Pressable style={styles.logoutBottom}>
+          <Text style={[styles.text, { color: "#fff" }]}>LOGOUT</Text>
+        </Pressable>
       </BlurView>
     </View>
   );
@@ -40,6 +80,7 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   blurContainer: {
+    width: "95%",
     padding: 20,
     margin: 10,
     textAlign: "center",
@@ -56,5 +97,34 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     fontWeight: "600"
+  },
+  itemContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomColor: "white",
+    borderBottomWidth: 1,
+    marginBottom: 25
+  },
+  editButton: {
+    width: "100%",
+    alignItems: "center",
+    borderBottomColor: "dodgerblue",
+    backgroundColor: "dodgerblue",
+    borderBottomWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 10
+  },
+  logoutBottom: {
+    width: "100%",
+    alignItems: "center",
+    borderBottomColor: "red",
+    backgroundColor: "red",
+    borderBottomWidth: 1,
+    marginBottom: 25,
+    padding: 10,
+    borderRadius: 10
   }
 });
